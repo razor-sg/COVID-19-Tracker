a65dbb62d94e129997362fc3fb566a6194bfadc7
razor-sg <razor.git@gmail.com>
2020-11-13 12:49:14 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MenuItem, Select, FormControl } from "@material-ui/core";
+import React, {useState, useEffect} from "react";
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  // https://disease.sh/v3/covid-19/countries
+
+  useEffect (() => {
+    // Code inside here will run once
+    // async -> send request -> wait -> do something
+    
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries").then((response) => response.json()).then((data) => {const countries = data.map((country) => (
+      {
+
+       name: country.country, // United States, United Kingdom
+       Value: country.countryInfo.iso2, // UK, USA, FR..
+
+      }));
+
+      setCountries(countries);
+  });
+ };
+ 
+ getCountriesData();
+}, []);
+
+  const onCountryChange = async (event) => {
+  const countryCode = event.target.value;
+
+  console.log("yooooo", countryCode);
+  setCountry(countryCode);
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1>COVID-19 TRACKER</h1>
+        <div classname="app__header">
+        <FormControl className="app__dropdown">
+          <Select varient = "outlined" onChange={onCountryChange} value={country}>
+            {/* Loop through all countries and show dropdown list of the options*/}
+
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+
+            {countries.map((country) => (
+               <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+               
+
+          </Select>
+        </FormControl>
+        </div>
+        
+
+        {/* Header */}
+        {/* Title + Select input dropdown field */}
+
+        {/* InforBoxs */}
+        {/* InforBoxs */}
+        {/* InforBoxs */}
+
+        {/* Table */}
+        {/* Graph */}
+
+        {/* Map */}
+
     </div>
   );
 }
